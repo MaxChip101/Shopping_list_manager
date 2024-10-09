8b1162365870afc75d561d7dd4a03127e6b630f2
MaxChip <1Max0Lag1@gmail.com>
2024-10-09 10:02:52 -0400

    first commit

---
+++ src/main.cpp
@@ -0,0 +1,159 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <cctype>
+#include <vector>
+
+bool isNumeric(const std::string& str) {
+    if(str[0] == '-') {
+        std::string str2 = "";
+
+        for(std::string::size_type i = 1; i < str.length(); i++) {
+            str2 += str[i];
+        }
+
+        str2 += "0";
+        return std::all_of(str2.begin(), str2.end(), ::isdigit);
+    } else {
+        return std::all_of(str.begin(), str.end(), ::isdigit);
+    }
+}
+
+// safe user input
+std::string input() {
+    std::string inp_str;
+    char ch;
+    while (std::cin.get(ch) && ch != '\n') {
+        inp_str += ch;
+    }
+    return inp_str;
+}
+
+std::string prompt(std::string text) {
+    // render the prompt text
+    std::cout << text << std::endl;
+    // displays the input text
+    std::cout << "(name) > " << std::flush;
+
+    std::string s;
+    s = input();
+
+    // gets user input
+    return s;
+}
+
+// prompt a number
+int prompt_int(std::string text) {
+    // render the prompt text
+    std::cout << text << std::endl;
+    // displays the input text
+    std::cout << "(number) > " << std::flush;
+
+    // gets user input
+    int i;
+    std::string s = input();
+
+    if(isNumeric(s)) {
+        i = stoi(s);
+    } else {
+        prompt_int(text);
+    }
+
+    return i;
+}
+
+char choice2(std::string text, std::string _choice1, std::string _choice2) {
+    // renders the prompt text with the choices
+    std::cout << text << std::endl;
+    std::cout << "   1) " << _choice1 << ", 2) " << _choice2 << std::endl;
+
+    // user input
+    std::string picked;
+
+    std::cout << " > " << std::flush;
+    picked = input();
+
+    // if input is within the choices then exit loop
+    if(picked[0] == '1' || picked[0] == '2') {
+        return picked[0];
+    } else {
+        choice2(text, _choice1, _choice2);
+    }
+
+    return '0';
+}
+
+char choice3(std::string text, std::string _choice1, std::string _choice2, std::string _choice3) {
+    // renders the prompt text with the choices
+    std::cout << text << std::endl;
+    std::cout << "   1) " << _choice1 << ", 2) " << _choice2 << ", 3) " << _choice3 << std::endl;
+
+    // user input
+    std::string picked;
+
+    std::cout << " > " << std::flush;
+    picked = input();
+
+    // if input is within the choices then exit loop
+    if(picked[0] == '1' || picked[0] == '2' || picked[0] == '3') {
+        return picked[0];
+    } else {
+        choice3(text, _choice1, _choice2, _choice3);
+    }
+
+    return '0';
+}
+
+void print_list(std::vector<std::string> list)
+{
+    std::cout << "Shopping List:" << std::endl;
+    for (size_t i = 0; i < list.size(); i++)
+    {
+        std::cout << "    " << i << ": " << list[i] << std::endl;
+    }
+    std::cout << std::endl;
+}
+
+int main()
+{
+    std::vector<std::string> shopping_list;
+    int pos;
+    while (true)
+    {
+        print_list(shopping_list);
+        char choice = choice3("What would you like to do to your shopping list?", "Add an item", "Change an item", "Remove an Item");
+        switch (choice)
+        {
+            case '1':
+                shopping_list.push_back(prompt("What is your item's name?"));
+                break;
+            
+            case '2':
+                pos = prompt_int("What item would you like to change?");
+                if(pos >= int(shopping_list.size()))
+                {
+                    std::cout << "That item does not exist" << std::endl;
+                }
+                else
+                {
+                    shopping_list[pos] = prompt("What would you like to change it to?");
+                }
+                break;
+
+            case '3':
+                pos = prompt_int("What item would you like to delete?");
+                if(pos >= int(shopping_list.size()))
+                {
+                    std::cout << "That item does not exist" << std::endl;
+                }
+                else
+                {
+                    shopping_list.erase(shopping_list.begin()+pos, shopping_list.begin()+pos+1);
+                }
+                break;
+        }
+    }
+
+
+    return 0;
+}
